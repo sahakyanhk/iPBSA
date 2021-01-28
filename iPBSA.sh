7a45a4155547ae0fa609332642e46db6f2bec5da
sahakyanhk <69894030+sahakyanhk@users.noreply.github.com>
2021-01-28 17:18:00 +0400

    Update iPBSA.sh

--- iPBSA.sh
+++ iPBSA.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# iPBSA is a script for docking results rescoring
+# !!! This is a beta version for reviewers, after manuscript revision, the major version will be realized. !!!
+# To use this script you need to have a receptor and ligands in their docked conformations in the PDB format with the following names and folders:
+
+# $DIR/iPDBS.sh    
+# $DIR/mols/all_ligantd*.pdb - a folder named "mols" with ligands in PDB format
+# $DIR/rec.pdb  - receptor named "rec.pdb"  
+
+# Once the files are prepared as it is described above, you need just activate and run the script.
+
+
 mkdir inp parm min gbsa pbsa
 cp rec.pdb parm
 
